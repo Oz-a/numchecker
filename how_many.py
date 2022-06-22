f5ef3581dbfc9d22314c7dff522bc361ba0b0152
Ozan Ahmet Ahmet <40339705+Oz-a@users.noreply.github.com>
2022-06-22 13:34:17 +0000

    created a dict function

---
+++ how_many.py
@@ -0,0 +1,17 @@
+
+def how_many(adict):
+  '''
+  input: dictionary, where all values are lists
+
+  returns the number of elements in the dictionary
+  '''
+  counter = 0
+  for k in adict.values():
+    for i in k:
+      counter += 1
+
+  return counter
+
+
+#Test = how_many({'B': [15], 'u': [10, 15, 5, 2, 6]})
+#print(Test)
