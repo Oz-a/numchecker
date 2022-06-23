a8c2f9d6d76b70cfeb2fd8e8a3e46a51a3f05aa1
Ozan Ahmet Ahmet <40339705+Oz-a@users.noreply.github.com>
2022-06-23 20:38:37 +0000

    finished hangman

--- hangman/ps3_hangman.py
+++ hangman/ps3_hangman.py
@@ -112,7 +112,40 @@ def hangman(secretWord):
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    # FILL IN YOUR CODE HERE...
+    print('Welcome to the game, Hangman!')
+    print('I am thinking of a word that is', len(secretWord), "letters long.")
+    mistakesMade = 0
+    lettersGuessed = []
+    
+    
+    while 8 - mistakesMade > 0:
+      if isWordGuessed(secretWord, lettersGuessed) == True:
+          print('------------')
+          print('Congratulations, you won!')
+          break
+      else:
+        	print('------------')
+        	print('You have', 8 - mistakesMade, 'guesses left.')
+        	print('Available letters:', getAvailableLetters(lettersGuessed))
+        	guess = str(input('Please guess a letter:')).lower()
+        	if guess in secretWord and guess not in lettersGuessed:
+        		lettersGuessed.append(guess)
+        		print('Good guess:', getGuessedWord(secretWord, lettersGuessed))
+        	elif guess in lettersGuessed:
+        		print("Oops! You've already guessed that letter:", getGuessedWord(secretWord, lettersGuessed))
+        	elif guess not in secretWord:
+        		print("Oops! That letter is not in my word:", getGuessedWord(secretWord, lettersGuessed))
+        		lettersGuessed.append(guess)
+        		mistakesMade += 1
+      if 8 - mistakesMade == 0:
+        	print('------------')
+        	print('Sorry, you ran out of guesses. The word was', secretWord)
+        	break
+      else:
+        	continue
+    
+
+        
 
 
 
@@ -123,5 +156,5 @@ def hangman(secretWord):
 # and run this file to test! (hint: you might want to pick your own
 # secretWord while you're testing)
 
-# secretWord = chooseWord(wordlist).lower()
-# hangman(secretWord)
+secretWord = chooseWord(wordlist).lower()
+hangman(secretWord)
